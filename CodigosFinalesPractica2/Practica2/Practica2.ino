9d456fb160b760d09b4851c0b391e015a5353d42
Anthony Maisincho Jivaja <77707111+Maydrummer@users.noreply.github.com>
2022-01-20 02:30:23 -0500

    Update Practica2.ino

--- CodigosFinalesPractica2/Practica2/Practica2.ino
+++ CodigosFinalesPractica2/Practica2/Practica2.ino
@@ -1,3 +1,17 @@
+//***************************************************//
+//***************************************************//
+//*****   Contador de RPM y controlador de      *****//
+//*****   de velocidad mediante la técnica      *****//
+//*****   PWM para un motor DC.                 *****//
+//*****                                         *****//
+//*****                                         *****//
+//***** by: Anthony Maisincho Jivaja,           *****//
+//*****     Ulbio Alejandro Sanjinés            *****//
+//*****                                         *****//
+//***************************************************//
+//***************************************************//
+
+
 #include <TimerOne.h>
 unsigned long counter=0;
 
@@ -34,40 +48,40 @@ void setup() {
 }
 
 void loop() {
-  variableSelector=digitalRead(selector);
+  variableSelector=digitalRead(selector); //Leemos la entrada del selector
   //Serial.print("PWM actual: ");
   //Serial.println(outputValue);
   delay(100);
-  if(variableSelector==0){
+  if(variableSelector==0){  //Si es cero podemos aumentar o disminuir el pwm de 1 en 1
     int estadoInicio=digitalRead(inicio);
     int estadoParada=digitalRead(parada);
     //Serial.println(outputValue);
-    if(estadoInicio==HIGH){
-      aux=1;
+    if(estadoInicio==HIGH){ //Si existe un cambio de estado de 0-1
+      aux=1; //Aux vale 1
         }
-    else if(aux==1 && estadoInicio==LOW){
-      outputValue+=1;
-      analogWrite(pwmout,outputValue);
-      aux=0;        
+    else if(aux==1 && estadoInicio==LOW){ //Si se deja de presionar y aux fue 1
+      outputValue+=1; //Se aumenta la variable PWM en 1
+      analogWrite(pwmout,outputValue); //Se manda el PWM al motor
+      aux=0; //Reseteamos la variable aux 
         }
-    else if(estadoParada==LOW){
-      aux2=1;
+    else if(estadoParada==LOW){ //Si ahora en el pulsador Normalmente cerrado se detecta un cambio de 1-0
+      aux2=1; //Aux2 se le asigna el 1
         }
-    else if(aux2==1 && estadoParada==HIGH){
-      outputValue-=1;
-      analogWrite(pwmout,outputValue);
-      aux2=0;        
+    else if(aux2==1 && estadoParada==HIGH){ //Si se deja de presionar el pulsador NC y si Aux2 vale 1 
+      outputValue-=1; //El valor de salida de PWM se disminuye en 1
+      analogWrite(pwmout,outputValue); //Se escribe el valor actual de PWM
+      aux2=0;  //Se reseta la variable
     }
-    else if(outputValue>29){
-      outputValue=30;
+    else if(outputValue>29){ //Si el valor de PWM sobrepasa 29
+      outputValue=30;  //Se limita el valor de salida de PWM a 30
       analogWrite(pwmout,outputValue);
       
       }
-    else if(outputValue< 0){
-      outputValue=0;     
+    else if(outputValue< 0){ //Si es mejor a cero
+      outputValue=0;   //Se limita a cero la variable de salida
       analogWrite(pwmout,outputValue);
       } }
-    else{
+    else{//Si el selector pasa a otro estado, se apaga el motor
       outputValue=0;
       analogWrite(pwmout,outputValue);
       }
